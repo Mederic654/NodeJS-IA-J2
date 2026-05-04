f6f9e4d4f61991b2d4c6af61aad58e17820f5ddc
Mederic654 <medericl@outlook.com>
2026-05-04 15:43:17 +0200

    Phase 4
    feat: streaming + multiprovider

--- chatbot-cli.js
+++ chatbot-cli.js
@@ -18,12 +18,53 @@ const history = [
     }
 ]
 
+const PROVIDERS = [
+    {
+        key: process.env.MISTRAL_API_KEY,
+        url:'https://api.mistral.ai/v1/chat/completions',
+        model:'mistral-small-latest',
+        type : 'Mistral'
+    },
+    {
+        key:process.env.GROQ_API_KEY,
+        url:'https://api.groq.com/openai/v1/chat/completions',
+        model:'llama-3.3-70b-versatile',
+        type : 'Groq'
+    }
+]
+
+let currentProvider = PROVIDERS[0];
+
+function switchProvider(name) 
+{
+    name = name.toLowerCase();
+
+    switch (true) {
+        case name.includes('mistral'):
+            currentProvider = PROVIDERS[0];
+            break;
+        case name.includes('groq'):
+            currentProvider = PROVIDERS[1];
+            break;
+        default:
+            //provider non supporté
+            console.log(name);
+            
+            return false;
+    }
+    return true;
+}
+
 console.log('Chatbot CLI - Phase 1. (Ctrl+C pour quitter)');
 
 //boucle question IA
 while (true) {
     const input = await question('Vous : ');
-    if (input === '/history') { console.log(history); continue; }
+    if (input.startsWith('/history')) { console.log(history); continue; }
+    if (input.startsWith('/provider') && switchProvider(input)) {
+        console.log(`Provider changé : (${currentProvider.type} ${currentProvider.model})`)
+        continue;
+    }
     await chatStream(input);
 }
 
@@ -34,14 +75,14 @@ async function chatStream(userMessage) {
         content: userMessage
     });
 
-    const response = await fetch('https://api.mistral.ai/v1/chat/completions', {
+    const response = await fetch(currentProvider.url, {
         method: 'POST',
         headers: {
-            'Authorization': `Bearer ${process.env.MISTRAL_API_KEY}`,
+            'Authorization': `Bearer ${currentProvider.key}`,
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            model: 'mistral-small-latest',
+            model: currentProvider.model,
             messages: history,
             temperature: 0.7,
             stream: true
