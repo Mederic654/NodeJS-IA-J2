a81b0bad749e31f8a99938f5d7fd9ed79120b76c
Mederic654 <medericl@outlook.com>
2026-05-04 14:51:51 +0200

    Phase 2
    feat: memory + conversation history

--- chatbot-cli.js
+++ chatbot-cli.js
@@ -3,39 +3,58 @@ import 'dotenv/config';
 import readline from 'node:readline';
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 function question(prompt) {
-  return new Promise(resolve => rl.question(prompt, resolve));
+    return new Promise(resolve => rl.question(prompt, resolve));
 }
 
+const history = [
+    {
+        role: 'system',
+        content: ''
+    }
+]
 
 console.log('Chatbot CLI - Phase 1. (Ctrl+C pour quitter)');
 
 while (true) {
-  const input = await question('Vous : ');
-  const reply = await askMistral(input);
-  console.log(`IA : ${reply}\n`);
+    const input = await question('Vous : ');
+    if (input === '/history') { console.log(history); continue;}
+
+    const reply = await askMistral(input);
+    console.log(`IA : ${reply}\n`);
 }
 
 async function askMistral(userMessage) {
-  const response = await fetch('https://api.mistral.ai/v1/chat/completions', {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${process.env.MISTRAL_API_KEY}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      model: 'mistral-small-latest',
-      messages: [
-        { role: 'user', content: userMessage }
-      ]
+
+    history.push({
+        role: 'user',
+        content: userMessage
+    });
+
+    const response = await fetch('https://api.mistral.ai/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${process.env.MISTRAL_API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: 'mistral-small-latest',
+            messages: history
+        })
+    });
+
+    const data = await response.json();
+
+    const assistantMessage = data.choices[0].message.content;
+
+    history.push({
+        role: 'assistant',
+        content: assistantMessage
     })
-  });
 
-  const data = await response.json();
-
-  return data.choices[0].message.content;
+    return assistantMessage;
 }
