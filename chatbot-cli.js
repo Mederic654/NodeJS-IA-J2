53944071cd8af354b1a174f4987bd13fa9c6719d
Mederic654 <medericl@outlook.com>
2026-05-04 16:25:52 +0200

    Phase 5

--- chatbot-cli.js
+++ chatbot-cli.js
@@ -11,6 +11,8 @@ function question(prompt) {
     return new Promise(resolve => rl.question(prompt, resolve));
 }
 
+const MAX_HISTORY = 5
+
 const history = [
     {
         role: 'system',
@@ -21,22 +23,21 @@ const history = [
 const PROVIDERS = [
     {
         key: process.env.MISTRAL_API_KEY,
-        url:'https://api.mistral.ai/v1/chat/completions',
-        model:'mistral-small-latest',
-        type : 'Mistral'
+        url: 'https://api.mistral.ai/v1/chat/completions',
+        model: 'mistral-small-latest',
+        type: 'Mistral'
     },
     {
-        key:process.env.GROQ_API_KEY,
-        url:'https://api.groq.com/openai/v1/chat/completions',
-        model:'llama-3.3-70b-versatile',
-        type : 'Groq'
+        key: process.env.GROQ_API_KEY,
+        url: 'https://api.groq.com/openai/v1/chat/completions',
+        model: 'llama-3.3-70b-versatile',
+        type: 'Groq'
     }
 ]
 
 let currentProvider = PROVIDERS[0];
 
-function switchProvider(name) 
-{
+function switchProvider(name) {
     name = name.toLowerCase();
 
     switch (true) {
@@ -49,7 +50,7 @@ function switchProvider(name)
         default:
             //provider non supporté
             console.log(name);
-            
+
             return false;
     }
     return true;
@@ -66,8 +67,45 @@ while (true) {
         continue;
     }
     await chatStream(input);
+    await compressHistory();
 }
 
+async function compressHistory() {
+    
+    if (history.length >= MAX_HISTORY) {
+
+        var histo = history.slice(1).map(m => `${m.role}:\n${m.content}`).join('\n');
+        var prompt = 'résume moi tout ce texte en 5 phrases maximum';
+
+        history.push({
+            role: 'user',
+            content: histo + prompt
+        })
+
+        const response = await fetch(currentProvider.url, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${currentProvider.key}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                model: currentProvider.model,
+                messages: history,
+                temperature: 0.3
+            })
+        });
+
+        const data = await response.json();
+        
+        const assistantMessage = data.choices[0].message.content;
+
+        history.splice(1, history.length);
+
+        history.push({role : 'assistant', content: assistantMessage});
+        
+        console.log('Contexte compressé');
+    }
+}
 async function chatStream(userMessage) {
 
     history.push({
@@ -96,14 +134,14 @@ async function chatStream(userMessage) {
     process.stdout.write('IA : ');
     //boucle pour affichage en stream
     while (true) {
- 
+
         const { done, value } = await reader.read();
         if (done) break;
 
-        
+
         const chunk = decoder.decode(value);
         const lines = chunk.split('\n').filter(line => line.startsWith('data: '));
-        
+
         for (const line of lines) {
 
             const jsonStr = line.slice(6);
@@ -111,7 +149,7 @@ async function chatStream(userMessage) {
             if (jsonStr.trim() === '[DONE]') continue;
 
             try {
-                      const delta = JSON.parse(jsonStr).choices[0]?.delta?.content;
+                const delta = JSON.parse(jsonStr).choices[0]?.delta?.content;
 
 
                 process.stdout.write(delta);
@@ -119,7 +157,7 @@ async function chatStream(userMessage) {
             }
             catch { }
         }
-        
+
     }
     process.stdout.write('\n\n');
 
